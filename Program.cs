c1ff23f561b3a1508455cd86faeb5554675b4d34
adventuriro <53796601+adventuriro@users.noreply.github.com>
2024-03-24 18:09:49 +0100

    modification 2

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, world!");
-        Console.WriteLine("This is Modification 1.");
+        Console.WriteLine("This is Modification 2.");
+        Console.WriteLine("Added a new feature.");
     }
 }
