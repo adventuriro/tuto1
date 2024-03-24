f0d2d01761e3b7ad666d2d95a51cd17082913208
adventuriro <53796601+adventuriro@users.noreply.github.com>
2024-03-24 20:06:13 +0100

    Revert "modification 3"
    This reverts commit 74d8c98096286980cec3bb41dea9531e67c5b961.

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, world!");
-        Console.WriteLine("This is Modification 3.");
-        Console.WriteLine("Fixed a bug.");
+        Console.WriteLine("This is Modification 2.");
+        Console.WriteLine("Added a new feature.");
     }
 }
